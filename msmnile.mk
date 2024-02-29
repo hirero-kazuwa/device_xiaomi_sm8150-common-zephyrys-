27a3d614fcee4a70a8e5306efd3321301e782ebd
hirero-kazuwa <hirero00@gmail.com>
2024-02-29 20:29:48 +0530

    sm8150-common: vayu: Camera Hals and Sepolicy fixup

--- msmnile.mk
+++ msmnile.mk
@@ -107,15 +107,20 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
-
-PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-service_64 \
+    android.frameworks.sensorservice@1.0.vendor \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    libstdc++_vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
+    libxml2 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    libshim_megvii \
+    libpiex_shim
+
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
@@ -216,6 +221,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
+
+# Camera
+PRODUCT_PACKAGES += \
+    libshim_megvii
 
 # Camera Legacy HIDL deps
 PRODUCT_PACKAGES += \
